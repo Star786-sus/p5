37358c9e6b3a053247a0da7c8f71ca2b6bae663d
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-04-26 14:15:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(700, 500);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 230, 150, 220, 200);
+
+    fill(105, 76, 168);
+    stroke(57, 38, 99);
+    circle(50, 50, 80);
+
+    circle(50, 430, 80);
+
+    circle(590, 50, 80);
+
+    circle(590, 430, 80);
+
+
+    fill(38, 53, 99);
+    stroke(38, 53, 99);
+    rect(90, 40, 460, 20);
+
+    rect(90, 420, 460, 20);
+
+    rect(40, 90, 20, 300);
+
+    rect(580, 90, 20, 300);
+
+}
+
+function take_snapshot()
+{
+    save('pictureFrame.png');
+}
